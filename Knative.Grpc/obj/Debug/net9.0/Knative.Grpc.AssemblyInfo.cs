09d87764835e93587e6fe81050d2da847304cf83
RedEye Developers <75036674+RSReswin@users.noreply.github.com>
2025-08-25 20:15:17 +0530

    backup files

--- Knative.Grpc/obj/Debug/net9.0/Knative.Grpc.AssemblyInfo.cs
+++ Knative.Grpc/obj/Debug/net9.0/Knative.Grpc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Knative.Grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54989e46d2a0267938b42f52679d1c65eab27ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Knative.Grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Knative.Grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
