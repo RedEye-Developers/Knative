15741d185a6350ac6de27de1ce899265315185eb
RedEye Developers <75036674+RSReswin@users.noreply.github.com>
2025-08-29 00:21:45 +0530

    Add REST-API with Scale to 0.

---
+++ Knative.Api/K6/LoadTest.js
@@ -0,0 +1,20 @@
+﻿import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 20,         
+  duration: '1m',  
+};
+
+export default function () {
+  const response = http.get('http://localhost:8080/weatherforecast', {
+    headers: {
+      'Host': 'knative-api.default.127.0.0.1.sslip.io'
+    }
+  });
+
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+}
